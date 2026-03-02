5a570fe8de29edd6020e2bd5b401791eec8e4621
barboscaio <caiiohenrique2004@gmail.com>
2026-03-02 14:18:51 -0300

    feat: configure react-hot-toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/index.routes';
+import { Toaster } from 'react-hot-toast';
 
 import './index.css';
 
 ReactDom.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<Toaster position='top-right' />
 		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
