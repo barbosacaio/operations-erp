c1ab66538b598e6e37dbddb2d997b58920a8f64b
barboscaio <caiiohenrique2004@gmail.com>
2026-03-06 13:50:57 -0300

    fix: workspace names have maxLength & proper validation messages

--- backend/src/modules/workspace/workspace.schema.ts
+++ backend/src/modules/workspace/workspace.schema.ts
@@ -8,13 +8,27 @@ export const listWorkspaceSchema = z.object({
 
 export const createWorkspaceSchema = z.object({
 	body: z.object({
-		name: z.string().min(3),
+		name: z
+			.string()
+			.min(3, {
+				message: 'Workspace name must have at least 3 characters',
+			})
+			.max(30, {
+				message: 'Workspace name can have up to 30 characters',
+			}),
 	}),
 });
 
 export const updateWorkspaceSchema = z.object({
 	body: z.object({
-		name: z.string().min(3),
+		name: z
+			.string()
+			.min(3, {
+				message: 'Workspace name must have at least 3 characters',
+			})
+			.max(30, {
+				message: 'Workspace name can have up to 30 characters',
+			}),
 	}),
 	params: z.object({
 		workspaceId: z.string().min(1),
